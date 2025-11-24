dad58726299029bd6acb9df744a1ac79d8904cee
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-11-24 14:39:19 +0900

    5.2 文字列操作の学習完了

---
+++ chap05/str_count.py
@@ -0,0 +1,8 @@
+msg = "にわにはにわにわとりがいる"
+
+print(msg.count("にわ"))
+print(msg.count("にわ", 3))
+print(msg.count("にわ", 3, 6))
+
+msg = "いちいちいちばいち"
+print(msg.count("いちいち"))
---
+++ chap05/str_strip.py
@@ -0,0 +1,5 @@
+msg = " 　こんにちは \t\n\r"
+
+print("「", msg.strip() + "」")
+print("「", msg.lstrip() + "」")
+print("「", msg.rstrip() + "」")
