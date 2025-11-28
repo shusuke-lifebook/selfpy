37a277b3540cc5c9ab0dffacf0028999e9a10d98
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-11-29 07:41:04 +0900

    6.3 辞書(dict)の学習完了 6. 標準ライブラリの学習完了

---
+++ chap06/dict_view.py
@@ -0,0 +1,16 @@
+d = {"apple": "りんご", "orange": "みかん", "melon": "メロン"}
+
+# 項目を列挙
+for item in d.items():
+    print(item)
+
+for key, value in d.items():
+    print(key, ":", value)
+
+# Keyを列挙
+for key in d.keys():
+    print(key)
+
+# 値を列挙
+for value in d.values():
+    print(value)
