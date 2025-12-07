9d83557e3de8cdcbf9809840f4184dcc3c81fc97
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-12-07 20:32:17 +0900

    chap11の学習完了

---
+++ chap11/practice/ex_meta_singleton.py
@@ -0,0 +1,12 @@
+class SingletonMeta(type):
+    def __init__(cls, name, bases, disc, **kwargs):
+        cls.__instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__call__(*args, **kwargs)
+        return cls.__instance
+
+
+class MySingleton(metaclass=SingletonMeta):
+    pass
